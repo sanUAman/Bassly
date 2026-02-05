d7d78f7b77ec7f452c13bbc374534828a3807794
sanUAman <dzivalovoleksandr@gmail.com>
2026-02-05 11:11:23 +0200

    Updated frontend.

--- static/js/how_it_works.js
+++ static/js/how_it_works.js
@@ -26,28 +26,3 @@ document.querySelector('.back-to-top-btn').addEventListener('click', function (e
         behavior: 'smooth',
     });
 });
-
-document.querySelectorAll(".faq-question").forEach(button => {
-    button.addEventListener("click", () => {
-        const item = button.closest(".faq-item");
-        const answer = item.querySelector(".faq-answer");
-
-        document.querySelectorAll(".faq-item").forEach(el => {
-            if (el !== item) {
-                el.classList.remove("active");
-                const a = el.querySelector(".faq-answer");
-                a.style.height = "0px";
-            }
-        });
-
-        const isOpen = item.classList.contains("active");
-
-        if (isOpen) {
-            answer.style.height = "0px";
-            item.classList.remove("active");
-        } else {
-            item.classList.add("active");
-            answer.style.height = answer.scrollHeight + "px";
-        }
-    });
-});
